88de35af548aa6efb701c5950222847fde58d43d
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-02-29 01:05:27 -0500

    Moves the MobX 'store' inside of the 'StoreProvider()' function to provide all the components in the app's hierarchy access to the items in this MobX 'store'

--- src/App.js
+++ src/App.js
@@ -5,14 +5,14 @@ import { useLocalStore, useObserver } from 'mobx-react';
 const StoreContext = React.createContext();
 
 // Wraps around all of the components in this app.
-const StoreProvider = ({ children }) => {};
-
-// MobX "store" does 2 things:
-// (1) A place where we store properties of state (the data we're tracking).
-// (2) Contains functions that will modify those state properties.
-const store = useLocalStore(() => ({
-	bugs: ['Centipede']
-}));
+const StoreProvider = ({ children }) => {
+	// MobX "store" does 2 things:
+	// (1) A place where we store properties of state (the data we're tracking).
+	// (2) Contains functions that will modify those state properties.
+	const store = useLocalStore(() => ({
+		bugs: ['Centipede']
+	}));
+};
 
 export default function App() {
 	return <main>Bugs!</main>;
