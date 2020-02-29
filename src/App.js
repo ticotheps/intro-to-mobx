5260e4206403beb0cd797e9bc1a2763dc687102c
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-02-29 01:01:01 -0500

    Defines the context of our store using the Context API that comes built-in to React

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { useLocalStore, useObserver } from 'mobx-react';
 
+// Defines the context of our store using the Context API that comes with React
+const StoreContext = React.createContext();
+
 // MobX "store" does 2 things:
 // (1) A place where we store properties of state (the data we're tracking).
 // (2) Contains functions that will modify those state properties.
