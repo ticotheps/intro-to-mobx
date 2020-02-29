c3fc725f4e922c3a091f45bf8dae9d937e277d30
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-02-29 01:03:31 -0500

    Creates a 'StoreProvider()' function that wraps around all of the components in our app and takes in a 'children' prop

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { useLocalStore, useObserver } from 'mobx-react';
 
-// Defines the context of our store using the Context API that comes with React
+// Defines the context of our store using the Context API that comes with React.
 const StoreContext = React.createContext();
+
+// Wraps around all of the components in this app.
+const StoreProvider = ({ children }) => {};
 
 // MobX "store" does 2 things:
 // (1) A place where we store properties of state (the data we're tracking).
