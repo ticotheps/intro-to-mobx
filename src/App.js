421a3987316f2e8d5be9ea0062f86587a1c54f1e
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-02-29 00:59:08 -0500

    Creates a MobX 'store' variable using the MobX 'useLocalStore()' function and stores a 'bugs' as a key with an array as the value

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import { useLocalStore, useObserver } from 'mobx-react';
 
+// MobX "store" does 2 things:
+// (1) A place where we store properties of state (the data we're tracking).
+// (2) Contains functions that will modify those state properties.
+const store = useLocalStore(() => ({
+	bugs: ['Centipede']
+}));
+
 export default function App() {
 	return <main>Bugs!</main>;
 }
